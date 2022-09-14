9d9a222d3e5756f611550a487fffd1ec8f7de5b3
Osama Tbaileh <osama.tb02@gmail.com>
2022-09-14 13:13:59 +0300

    function intermediate 1 update

--- PythonAssignments/Ass.8-FunctoinIntermediateI/function_intermediate_I.py
+++ PythonAssignments/Ass.8-FunctoinIntermediateI/function_intermediate_I.py
@@ -4,8 +4,8 @@ def randInt(min=0, max=100):
     if min>max:
         return False
     else:
-        number = random.random()*(max-min) + min
-        number = round (num)
-        return num
+        num = random.random()*(max-min) + min
+        num = round (num)
+        return number
 result = randInt(min=50, max=70)
 print (result)
