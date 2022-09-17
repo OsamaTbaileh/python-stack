d75646c2469044d0b14ff0fd958d1e9c71a08038
Osama Tbaileh <osama.tb02@gmail.com>
2022-09-17 13:37:03 +0300

    "BankAccount" update

--- PythonAssignments/Ass.12-BankAccount/bank_account.py
+++ PythonAssignments/Ass.12-BankAccount/bank_account.py
@@ -1,7 +1,6 @@
 
 class BankAccount:
-    def __init__(self, name, balance=0, int_rate=0.01):
-        self.name = name
+    def __init__(self, balance=0, int_rate=0.01):
         self.balance = balance
         self.int_rate = int_rate
 
@@ -18,7 +17,7 @@ class BankAccount:
         return self
 
     def diplay_account_info(self):
-        print(f"The balance in {self.name} account is: {self.balance}")
+        print(f"The balance in this account is: {self.balance}$")
 
     def yield_interest(self):
         if self.balance > 0:
@@ -27,8 +26,8 @@ class BankAccount:
             print(f"Opreration failed - No enough balance in your account")
         return self
 
-account_1 = BankAccount ("Osama Tbaileh", 1000)
-account_2 = BankAccount ("Hasan Sadaqa", 2000)
+account_1 = BankAccount ()
+account_2 = BankAccount ()
 
 account_1.make_deposite(100) .make_deposite(200) .make_deposite(
         300) .make_withdrawal(300) .yield_interest() .diplay_account_info()
