5d4e6242dd36742802d9ee08f10049560d2a0876
Osama Tbaileh <osama.tb02@gmail.com>
2022-09-22 22:58:28 +0300

    "Dojo Survey" Assignment

--- flask/flask_fundamentals/Ass.19-DojoSurvey/dojo_survey.py
+++ flask/flask_fundamentals/Ass.19-DojoSurvey/dojo_survey.py
@@ -8,20 +8,19 @@ def show_main_page():
 
 @app.route('/result', methods=['POST'])          
 def create_ursers():
-    name_from_form = request.form['name']
-    location_from_form = request.form['location']
-    language_from_form = request.form['language']
-    comment_from_form = request.form['comment']
-    # gender_from_form = request.form['flexRadioDefault']
-    # checkbox_from_form = request.form['checkbox']
-    return render_template("result.html",
-        name_on_template = name_from_form,
-        location_on_template = location_from_form,
-        language_on_template = language_from_form,
-        comment_on_template = comment_from_form,  
-        # gender_on_template = gender_from_form,
-        # checkbox_on_template = checkbox_from_form
-        )
+
+    results= [
+    {"name": request.form['name']},
+    {"location": request.form['location']},
+    {"language": request.form['language']},
+    {"comment": request.form['comment']},
+    {"gender": request.form['flexRadioDefault']},
+    # {"checkbox": "NO"}
+]
+    # if request.form['checkbox'] == "on":
+    #     results.append={"checkbox": "OK"}
+
+    return render_template("result.html", results_template = results)
 
 @app.errorhandler(404)
 def notValidRoute(e):
