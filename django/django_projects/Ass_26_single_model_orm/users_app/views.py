a6638ba59109bf9470801b25e8ba6efd2db4bec9
Osama Tbaileh <osama.tb02@gmail.com>
2022-09-25 21:18:31 +0300

    "Users with Templates" Assignment

--- django/django_projects/Ass_26_single_model_orm/users_app/views.py
+++ django/django_projects/Ass_26_single_model_orm/users_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.http import JsonResponse
+from .models import User
 
-# Create your views here.
+def root(request):
+    context = {
+        "all_the_users": User.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def new_user_method(request):
+    new_user = User.objects.create(
+    first_name=request.POST['first_name'], 
+    last_name = request.POST['last_name'], 
+    email_address=request.POST['email'],
+    age=request.POST['age'])
+    print(new_user)
+    return redirect('/')
